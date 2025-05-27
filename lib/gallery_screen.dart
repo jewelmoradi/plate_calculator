b411f5666e1e78bc4050e21efc290a84df861bc7
Negin Moradi <neginmoradi@gmail.com>
2025-05-27 18:11:15 +0330

    added the camera and gallery screens, implemented initial gallery screen logic

--- lib/gallery_screen.dart
+++ lib/gallery_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({super.key});
@@ -8,8 +10,35 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
+  File? _image;
+
   @override
+  void initState() {
+    super.initState();
+    pickGalleryImage();
+  }
+
+  // Asynchronous function that waits for the user to select their picture from their device
+  Future<void> pickGalleryImage() async {
+    final pickedFile = await ImagePicker().pickImage(
+        source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+    @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Gallery Image')
+      ),
+      body: Center(
+        child: _image != null ? Image.file(_image!) : Text('No image selected'),
+      ),
+    );
   }
 }
