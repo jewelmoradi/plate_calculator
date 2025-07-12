de820f7d46b63ecaa5a453ab7acb3b42ae60f03e
Negin Moradi <neginmoradi@gmail.com>
2025-07-12 23:59:17 +0330

    Added a function to analyze the received image

--- python_backend/app.py
+++ python_backend/app.py
@@ -5,6 +5,14 @@ import io
 app = Flask(__name__)
 
 
+# dummy function to check if the image is analyzed
+def analyze_image(image):
+    if image.size[0] > 500:
+        return "Large image"
+    else:
+        return "Small image"
+
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     if 'image' not in request.files:
@@ -14,10 +22,9 @@ def analyze():
     image_bytes = image_file.read()
     image = Image.open(io.BytesIO(image_bytes))
 
-    # --- Your AI model would go here ---
-    result = {'analyze': 'dummy result'}  # Temporary response
+    prediction = analyze_image(image)
 
-    return jsonify(result)
+    return jsonify({'prediction': prediction})
 
 
 if __name__ == '__main__':
